b56de9cb2f66f6bd582faf77aab9fee20906ea80
arisu6804 <arisu6804@gmail.com>
2026-06-12 17:10:47 +0530

    feat: add contract error enum with init variants

---
+++ src/error.rs
@@ -0,0 +1,12 @@
+use soroban_sdk::contracterror;
+
+/// Errors that can be returned by the YieldVault contract.
+#[contracterror]
+#[derive(Copy, Clone, Debug, Eq, PartialEq, PartialOrd, Ord)]
+#[repr(u32)]
+pub enum Error {
+    /// The contract has already been initialized.
+    AlreadyInitialized = 1,
+    /// The contract has not been initialized yet.
+    NotInitialized = 2,
+}

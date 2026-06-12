975e0a1a20d2c648b1fd0ffbd81b96dabbbf4c1b
arisu6804 <arisu6804@gmail.com>
2026-06-12 17:13:07 +0530

    feat: add withdraw event helper

--- src/events.rs
+++ src/events.rs
@@ -6,3 +6,10 @@ pub fn deposit(env: &Env, from: &Address, assets: u128, shares: u128) {
     let topics = (Symbol::new(env, "deposit"), from.clone());
     env.events().publish(topics, (assets, shares));
 }
+
+/// Publishes a `withdraw` event recording that `from` burned `shares` vault
+/// shares to redeem `assets` of the underlying token.
+pub fn withdraw(env: &Env, from: &Address, shares: u128, assets: u128) {
+    let topics = (Symbol::new(env, "withdraw"), from.clone());
+    env.events().publish(topics, (shares, assets));
+}
